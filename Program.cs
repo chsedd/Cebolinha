e2e4680f4cd360df3708bf991e2429315759ebc8
chsedd <83226177+chsedd@users.noreply.github.com>
2025-03-30 14:36:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string frase;
+        Console.Write("Digite uma frase: ");
+        frase = Console.ReadLine();
+        
+        string fraseCebolinha = frase.Replace("r", "l").Replace("R", "L");
+        
+        Console.WriteLine("Cebolinha diz: " + fraseCebolinha);
+    }
+}
